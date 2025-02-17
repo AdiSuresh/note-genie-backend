3564adc37ba0ca13e993d0d76c9b7cee6df3adc9
ADITHYA SURESH <adithyasuresh201@gmail.com>
2025-02-17 11:46:02 +0530

    create query api

--- app/main.py
+++ app/main.py
@@ -1,11 +1,35 @@
 from fastapi import FastAPI
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from app.models.query_request import QueryRequest
 
 app = FastAPI()
 
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
+
+model = OllamaLLM(model='deepseek-r1:1.5b')
+
+prompt = ChatPromptTemplate.from_template(template=template)
+
+chain = prompt | model
+
 @app.get("/")
-def read_root():
+def get_root():
     return {"message": "Hello, FastAPI!"}
 
-@app.get("/hello/{name}")
-def read_item(name: str):
-    return {"message": f"Hello, {name}!"}
+@app.post('/query')
+def post_query(request: QueryRequest):
+    result = chain.invoke({
+        'context': request.context,
+        'question': request.question
+    })
+    
+    return {'result': result}
