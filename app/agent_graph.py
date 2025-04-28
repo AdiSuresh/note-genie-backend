7286ab9f929edaf942b19817a76b0caa0abcc921
Adithya Suresh <adithyasuresh201@gmail.com>
2025-04-28 13:19:27 +0530

    update system message + add create graph static method + add memory param.

--- app/agent_graph.py
+++ app/agent_graph.py
@@ -1,12 +1,13 @@
+from __future__ import annotations
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage
 from typing import Annotated, TypedDict
 from langgraph.graph import StateGraph, START
+from langgraph.graph.state import CompiledStateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.checkpoint.memory import MemorySaver
 
-system_message = SystemMessage('You are a helpful assistant.')
+system_message = SystemMessage('You are a helpful assistant that can produce human-like responses.')
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -18,17 +19,23 @@ prompt = ChatPromptTemplate.from_messages(
 class AgentState(TypedDict):
     messages: Annotated[list, add_messages]
 
+
 class AgentGraph(StateGraph):
+    @staticmethod
+    def create_graph(llm, tools, memory) -> CompiledStateGraph:
+        return AgentGraph(llm, tools, memory).graph
+    
     def chatbot(self, state: AgentState):
         chain = prompt | self.llm
         message = chain.invoke({'messages': state['messages']})
+        assert len(message.tool_calls) <= 1
         return {'messages': [message]}
     
-    def __init__(self, llm, tools):
+    def __init__(self, llm, tools, memory):
         super().__init__(state_schema=AgentState)
 
         self.llm = llm
-        
+
         self.add_node('chatbot', self.chatbot)
 
         tool_node = ToolNode(tools=tools)
@@ -40,8 +47,7 @@ class AgentGraph(StateGraph):
         )
         self.add_edge('tools', 'chatbot')
         self.add_edge(START, 'chatbot')
-        
-        memory = MemorySaver()
+
         self.graph = self.compile(
             checkpointer=memory,
         )
