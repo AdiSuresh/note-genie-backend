3624ff1c652fd720a7f65c03f8c2bfa729790c52
Adithya Suresh <adithyasuresh201@gmail.com>
2025-05-04 13:12:21 +0530

    include new fields in settings for auth + make field names lower-case

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,11 +3,16 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = 'Note Genie Backend'
-    DATABASE_NAME: str
-    DATABASE_URI: str
-    LLM_API_KEY: str
+    database_name: str
+    database_uri: str
+    llm_api_key: str
+
+    jwt_secret: str
+    jwt_algorithm: str = 'HS256'
+    access_token_expire_minutes: int = 60 * 24 * 10
 
     class Config:
         env_file = '.env'
+        case_sensitive = False
 
 settings = Settings()
